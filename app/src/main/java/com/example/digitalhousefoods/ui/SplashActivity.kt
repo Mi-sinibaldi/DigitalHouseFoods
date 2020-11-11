1528aeb82c4ee6475da869d3907feb5cc7161e91
mi-sinibaldi <mia_araujo92@hotmail.com>
2020-11-11 15:09:18 -0300

    implementacao splash

--- app/src/main/java/com/example/digitalhousefoods/ui/SplashActivity.kt
+++ app/src/main/java/com/example/digitalhousefoods/ui/SplashActivity.kt
@@ -1,13 +1,25 @@
 package com.example.digitalhousefoods.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 import com.example.digitalhousefoods.R
+
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        val handler = Handler()
+        handler.postDelayed({ showLoginActivity() }, 2000)
+    }
 
-}}
+    private fun showLoginActivity() {
+        val intent = Intent(
+            this@SplashActivity, LoginActivity::class.java
+        )
+        startActivity(intent)
+        finish()
+    }
+}
