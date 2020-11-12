9d6c032a27f1bd4a545b20ac315fd73a3386451a
mi-sinibaldi <mia_araujo92@hotmail.com>
2020-11-12 13:18:51 -0300

    implementacao das validoes login e register

--- app/src/main/java/com/example/digitalhousefoods/ui/LoginActivity.kt
+++ app/src/main/java/com/example/digitalhousefoods/ui/LoginActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.example.digitalhousefoods.R
@@ -29,8 +30,17 @@ class LoginActivity : AppCompatActivity() {
 
     private fun clickBtnLogin() {
         val btnLogin: Button = findViewById<View>(R.id.btnLogin) as Button
+        val editTexUserLogin: EditText = findViewById(R.id.editTexUserLogin)
+        val editTextPasswordLogin: EditText = findViewById(R.id.editTextPasswordLogin)
+
         btnLogin.setOnClickListener {
-            startActivity(Intent(this@LoginActivity, PrincipalActivity::class.java))
+            if (editTexUserLogin.text.isNotEmpty() && editTextPasswordLogin.text.isNotEmpty()) {
+                startActivity(Intent(this@LoginActivity, PrincipalActivity::class.java))
+            } else {
+                Toast.makeText(this, "Email e senha não podem ser vazios", Toast.LENGTH_SHORT)
+                    .show()
+
+            }
         }
     }
 
@@ -39,15 +49,5 @@ class LoginActivity : AppCompatActivity() {
         btnRegister.setOnClickListener {
             startActivity(Intent(this@LoginActivity, RegisterActivity::class.java))
         }
-    }
-
-    fun getInformationUser(): User {
-        val editTexUserLogin: EditText = findViewById(R.id.editTexUserLogin)
-        val editTextPasswordLogin: EditText = findViewById(R.id.editTextPasswordLogin)
-
-        val username = editTexUserLogin.text.toString()
-        val password = editTextPasswordLogin.text.toString()
-
-        return User(1, username, password)
     }
 }
--- app/src/main/java/com/example/digitalhousefoods/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/digitalhousefoods/ui/RegisterActivity.kt
@@ -1,7 +1,12 @@
 package com.example.digitalhousefoods.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.example.digitalhousefoods.R
 
@@ -15,5 +20,28 @@ class RegisterActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
 
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        clickBtnRegister()
+    }
+
+    private fun clickBtnRegister() {
+        val buttonRegister: Button = findViewById<View>(R.id.buttonRegister) as Button
+        val editTextNameRegister: EditText = findViewById(R.id.editTextNameRegister)
+        val editTextEmailRegister: EditText = findViewById(R.id.editTextEmailRegister)
+        val editTextPasswordRegister: EditText = findViewById(R.id.editTextPasswordRegister)
+        val editTextReapetPasswordRegister: EditText =
+            findViewById(R.id.editTextReapetPasswordRegister)
+
+        buttonRegister.setOnClickListener {
+            if (editTextNameRegister.text.isNotEmpty() && editTextEmailRegister.text.isNotEmpty()
+                && editTextPasswordRegister.text.isNotEmpty() && editTextReapetPasswordRegister.text.isNotEmpty()
+            ) {
+                startActivity(Intent(this@RegisterActivity, LoginActivity::class.java))
+            }else{
+                Toast.makeText(this, "Todos os campos devem ser preenchidos!", Toast.LENGTH_SHORT)
+                    .show()
+            }
+
+        }
     }
 }
